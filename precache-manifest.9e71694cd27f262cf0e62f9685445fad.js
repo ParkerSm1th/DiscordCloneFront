7217b9d5e941d76f83397abca3d1506c3b6170ff
Parker Smith <parkersmith@Parkers-MacBook-Pro.local>
2021-01-31 01:33:17 -0800

    Updates

---
+++ precache-manifest.9e71694cd27f262cf0e62f9685445fad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb57265237583f29da7006935c1d51f2",
+    "url": "/DiscordCloneFront/index.html"
+  },
+  {
+    "revision": "5c7ef67843334fe8aef9",
+    "url": "/DiscordCloneFront/static/css/2.b0f64131.chunk.css"
+  },
+  {
+    "revision": "3dace84ed8e39046ddc0",
+    "url": "/DiscordCloneFront/static/css/main.f363e881.chunk.css"
+  },
+  {
+    "revision": "5c7ef67843334fe8aef9",
+    "url": "/DiscordCloneFront/static/js/2.1de838ae.chunk.js"
+  },
+  {
+    "revision": "3dace84ed8e39046ddc0",
+    "url": "/DiscordCloneFront/static/js/main.92a9c8a0.chunk.js"
+  },
+  {
+    "revision": "24d4435f4b07127e82e6",
+    "url": "/DiscordCloneFront/static/js/runtime~main.d7617bff.js"
+  }
+]);
